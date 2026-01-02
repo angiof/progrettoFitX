3f5bb6d25a53af95a2d5dbb16b1e0bf54a3c8377
Angelo Ferretti <126796176+AngeloFerretti@users.noreply.github.com>
2026-01-02 15:27:54 +0100

    aggiunta fix alla schermata dashboard da completare

--- wear/build/tmp/kapt3/stubs/debug/com/app/fityo/dominio/HomeMenuAction.java
+++ wear/build/tmp/kapt3/stubs/debug/com/app/fityo/dominio/HomeMenuAction.java
@@ -1,11 +1,12 @@
 package com.app.fityo.dominio;
 
-@kotlin.Metadata(mv = {2, 1, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\u0007\b\u0086\u0081\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\t\b\u0002\u00a2\u0006\u0004\b\u0002\u0010\u0003j\u0002\b\u0004j\u0002\b\u0005j\u0002\b\u0006j\u0002\b\u0007\u00a8\u0006\b"}, d2 = {"Lcom/app/fityo/dominio/HomeMenuAction;", "", "<init>", "(Ljava/lang/String;I)V", "CREATE_SCHEDE", "OPEN_SCHEDE", "VIEW_STATS", "MUSCLE_COMPARE", "wear_debug"})
+@kotlin.Metadata(mv = {2, 1, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0010\n\u0002\b\b\b\u0086\u0081\u0002\u0018\u00002\b\u0012\u0004\u0012\u00020\u00000\u0001B\t\b\u0002\u00a2\u0006\u0004\b\u0002\u0010\u0003j\u0002\b\u0004j\u0002\b\u0005j\u0002\b\u0006j\u0002\b\u0007j\u0002\b\b\u00a8\u0006\t"}, d2 = {"Lcom/app/fityo/dominio/HomeMenuAction;", "", "<init>", "(Ljava/lang/String;I)V", "CREATE_SCHEDE", "OPEN_SCHEDE", "VIEW_STATS", "MUSCLE_COMPARE", "TUTOR_MODE", "wear_debug"})
 public enum HomeMenuAction {
     /*public static final*/ CREATE_SCHEDE /* = new CREATE_SCHEDE() */,
     /*public static final*/ OPEN_SCHEDE /* = new OPEN_SCHEDE() */,
     /*public static final*/ VIEW_STATS /* = new VIEW_STATS() */,
-    /*public static final*/ MUSCLE_COMPARE /* = new MUSCLE_COMPARE() */;
+    /*public static final*/ MUSCLE_COMPARE /* = new MUSCLE_COMPARE() */,
+    /*public static final*/ TUTOR_MODE /* = new TUTOR_MODE() */;
     
     HomeMenuAction() {
     }
